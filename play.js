622bffa5a339c0a05f0511747aec5242bf14d272
stoutjmBYU <stoutjm@byu.edu>
2024-04-23 15:34:51 -0600

    It is playable! When you click the button a timer starts for 5 seconds and your score goes up. When the timer is up the button disables and grays out. Next, I will implement the game saving that to your local database.

--- play.js
+++ play.js
@@ -1,14 +1,36 @@
 
 class Game {
     constructor() {
+        this.score = 0;
+        // 0=not started, 1=playing, 2=ready to restart
+        this.game_mode = 0;
         const playerNameEl = document.querySelector('.player-name');
         playerNameEl.textContent = localStorage.getItem('userName') ?? 'MISSINGNO.';
         // This is the only button, multipurpose.
-        let button = document.querySelector('.game-button')
-        button.textContent = "INCREASE POWER"
+        this.button = document.querySelector('.game-button')
     }
     onButtonPress() {
+        // Reload the page if the game is ready for restart.
+        if (this.game_mode === 2) {
+            location.reload();
+        }
+        // Start the game if it hasn't started yet.
+        if (this.game_mode === 0) {
+            this.button.textContent = "INCREASE POWER"
+            setTimeout(this.timeup.bind(this), 5000);
+        }
         console.log('button pressed.')
+        this.score += 1;
+        this.updateScore();
+    }
+    updateScore() {
+        const scoreEl = document.querySelector('#score');
+        scoreEl.textContent = this.score;
+      }
+    timeup() {
+        this.button.textContent = "Time's up."
+        this.button.disabled = true;
+        // TODO save current score to the database.
     }
 }
 
