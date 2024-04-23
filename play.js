d4eccbe678d5fc568503977ce15ec840c69b8252
stoutjmBYU <stoutjm@byu.edu>
2024-04-23 15:41:53 -0600

    Dynamic instructions I made the instructions change depending on what part of the game you are at. Once the game is over they tell you to refresh the page or checkout your high scores.

--- play.js
+++ play.js
@@ -17,7 +17,9 @@ class Game {
         // Start the game if it hasn't started yet.
         if (this.game_mode === 0) {
             this.button.textContent = "INCREASE POWER"
-            setTimeout(this.timeup.bind(this), 5000);
+            const instruct_text = document.querySelector('#instruct-text')
+            instruct_text.textContent = "GO! GO! GOOO!"
+            setTimeout(this.timeup.bind(this), 5000);            
         }
         console.log('button pressed.')
         this.score += 1;
@@ -30,6 +32,8 @@ class Game {
     timeup() {
         this.button.textContent = "Time's up."
         this.button.disabled = true;
+        const instruct_text = document.querySelector('#instruct-text')
+        instruct_text.textContent = "Great job! To see your score on the global boards, go to the scores tab. Or refresh the page to play again."
         // TODO save current score to the database.
     }
 }
