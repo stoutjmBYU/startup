7febdf467fd5349cdc17dff133b4bffdd141e68d
stoutjmBYU <stoutjm@byu.edu>
2024-04-23 14:34:37 -0600

    Play button script and look Cut out the reset button, now there's just one button and it is very big and bold so you want to click it. Practiced changing the text in script on the button so that I can make it multipurpose for my game. Pressing the button actually activates a script (right now it just counts)

--- play.js
+++ play.js
@@ -3,6 +3,12 @@ class Game {
     constructor() {
         const playerNameEl = document.querySelector('.player-name');
         playerNameEl.textContent = localStorage.getItem('userName') ?? 'MISSINGNO.';
+        // This is the only button, multipurpose.
+        let button = document.querySelector('.game-button')
+        button.textContent = "INCREASE POWER"
+    }
+    onButtonPress() {
+        console.log('button pressed.')
     }
 }
 
