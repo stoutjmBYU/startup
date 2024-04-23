fed86d5f2a4e34aaf5d88305e1708d6f2221f935
stoutjmBYU <stoutjm@byu.edu>
2024-04-23 16:30:22 -0600

    Finally got the score saving system working I recycled most of simon's code but for some reason figuring it all out and getting it into mine was tough.

--- play.js
+++ play.js
@@ -1,41 +1,82 @@
-
 class Game {
+
     constructor() {
         this.score = 0;
-        // 0=not started, 1=playing, 2=ready to restart
-        this.game_mode = 0;
+        // 0=not started, 1=started
+        this.game_started = false;
         const playerNameEl = document.querySelector('.player-name');
-        playerNameEl.textContent = localStorage.getItem('userName') ?? 'MISSINGNO.';
+        playerNameEl.textContent = this.getPlayerName();
         // This is the only button, multipurpose.
-        this.button = document.querySelector('.game-button')
+        this.button = document.querySelector('.game-button');
     }
+
+    getPlayerName() {
+        return localStorage.getItem('userName') ?? 'MISSINGNO.';
+    }
+
     onButtonPress() {
-        // Reload the page if the game is ready for restart.
-        if (this.game_mode === 2) {
-            location.reload();
-        }
         // Start the game if it hasn't started yet.
-        if (this.game_mode === 0) {
+        if (this.game_started === false) {
             this.button.textContent = "INCREASE POWER"
             const instruct_text = document.querySelector('#instruct-text')
             instruct_text.textContent = "GO! GO! GOOO!"
-            setTimeout(this.timeup.bind(this), 5000);            
+            setTimeout(this.timeup.bind(this), 5000);         
+            this.game_started = true;
         }
         console.log('button pressed.')
         this.score += 1;
-        this.updateScore();
+        this.setScore();
     }
-    updateScore() {
+
+    setScore() {
         const scoreEl = document.querySelector('#score');
         scoreEl.textContent = this.score;
       }
+
     timeup() {
         this.button.textContent = "Time's up."
         this.button.disabled = true;
         const instruct_text = document.querySelector('#instruct-text')
         instruct_text.textContent = "Great job! To see your score on the global boards, go to the scores tab. Or refresh the page to play again."
-        // TODO save current score to the database.
+        
+        this.saveScore();
     }
+
+    saveScore(score) {
+        const userName = this.getPlayerName();
+        let scores = [];
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+          scores = JSON.parse(scoresText);
+        }
+        scores = this.updateScores(userName, this.score, scores);
+    
+        localStorage.setItem('scores', JSON.stringify(scores));
+      }
+    
+      updateScores(userName, score, scores) {
+        const date = new Date().toLocaleDateString();
+        const newScore = { name: userName, score: score, date: date };
+    
+        let found = false;
+        for (const [i, prevScore] of scores.entries()) {
+          if (score > prevScore.score) {
+            scores.splice(i, 0, newScore);
+            found = true;
+            break;
+          }
+        }
+    
+        if (!found) {
+          scores.push(newScore);
+        }
+    
+        if (scores.length > 10) {
+          scores.length = 10;
+        }
+    
+        return scores;
+      }
 }
 
 const game = new Game();
