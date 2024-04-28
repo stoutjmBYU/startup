d7e44f65a68ce1af2312fb7d95fb7bec7b335ff6
stoutjmBYU <stoutjm@byu.edu>
2024-04-28 14:19:01 -0600

    Studied and commented index.js to understand it more thouroughly

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+// Creates an instance of the express application 
 const express = require('express');
 const app = express();
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+// NOTE: I changed this to port 4000, hopefully i did that correctly.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-// JSON body parsing using built-in middleware
+// JSON body parsing using built-in middleware.
+// Makes JSON easy to parse.
 app.use(express.json());
 
 // Serve up the frontend static content hosting
+// In other words, make everything in the public directory download to client
 app.use(express.static('public'));
+
+/********************* API ROUTER */
 
 // Router for service endpoints
 const apiRouter = express.Router();
@@ -25,6 +31,8 @@ apiRouter.post('/score', (req, res) => {
   res.send(scores);
 });
 
+/********************* App presets */
+
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
@@ -33,3 +41,29 @@ app.use((_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+/********************** Define score funcs */
+
+// updateScores considers a new score for inclusion in the high scores.
+// The high scores are saved in memory and disappear whenever the service is restarted.
+let scores = [];
+function updateScores(newScore, scores) {
+  let found = false;
+  for (const [i, prevScore] of scores.entries()) {
+    if (newScore.score > prevScore.score) {
+      scores.splice(i, 0, newScore);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    scores.push(newScore);
+  }
+
+  if (scores.length > 10) {
+    scores.length = 10;
+  }
+
+  return scores;
+}
